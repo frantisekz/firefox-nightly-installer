c4dba85e0dfeaca401eb0f11f6ad8753f158ed5d
hbarn <hbarn@users.noreply.github.com>
2017-09-29 18:09:33 -0400

    Update firefox-nightly-installer.sh
    updated to new Mozilla.org ftp path
    updated to new Firefox nightly icon, linked to an icon at wikimedia
    updated .desktop file Name to easier to understand "Firefox Nightly"
    removed duplicative .desktop file in home directory

--- firefox-nightly-installer.sh
+++ firefox-nightly-installer.sh
@@ -8,7 +8,7 @@ file="Nightly.desktop"
 mkdir $HOME/.tmp-install/
 cd $HOME/.tmp-install/
 echo "Downloading firefox nightly..."
-wget http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-"$ver".0a1.en-US.linux-"$arch".tar.bz2
+wget https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-"$ver".0a1.en-US.linux-"$arch".tar.bz2
 tar -xf firefox-"$ver".0a1.en-US.linux-"$arch".tar.bz2
 
 sudo mkdir -p /opt/firefox/nightly
@@ -17,12 +17,12 @@ mv firefox/* /opt/firefox/nightly
 sudo ln -s /opt/firefox/nightly/firefox /usr/local/bin/nightly
 
 #Launcher
-	wget http://img.zatloukalu.eu/nightly.png -O $HOME/.mozilla/nightly.png
+	wget https://upload.wikimedia.org/wikipedia/commons/5/5c/Firefox_Nightly_Logo%2C_2017.png -O $HOME/.mozilla/nightly.png 
 	echo \[Desktop Entry\] >> "$file"
 	echo Type=Application >> "$file"
 	echo Terminal=false >> "$file"
 	echo "Categories=GNOME;GTK;Network;WebBrowser;"  >> "$file"
-	echo Name=nightly-"$file" >> "$file"
+	echo Name=Firefox Nightly >> "$file"
 	echo Icon=$HOME/.mozilla/nightly.png >> "$file"
 	echo Exec=nightly %u >> "$file"
 
@@ -30,7 +30,7 @@ chmod +x "$file"
 
 echo "Done, now copying the launcher to the system level" 
 sudo cp "$file" /usr/share/applications
-sudo cp "$file" $HOME/
-echo "Launcher is available in your home folder too"
+#sudo cp "$file" $HOME/ #don't need
+#echo "Launcher is available in your home folder too"
 cd ..
 rm -R $HOME/.tmp-install/
